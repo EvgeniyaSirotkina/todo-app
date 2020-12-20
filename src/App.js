7b109ed7acbac42c33262aa73217004198adec37
YauheniyaSirotkina <zsirotkina@gmail.com>
2020-12-20 19:18:34 +0300

    removed unnecessary files/lines + added tasks with rendering main block with tasks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Task from './components/Task';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      tasks: [
+        { id: 0, title: 'Create ReactJS ToDo App', done: false },
+        { id: 1, title: 'Buy gifts for the New Year holidays', done: true }
+      ]
+    }
+  }
+
+  render() {
+    const { tasks } = this.state;
+    const activeTasks = tasks.filter(task => !task.done);
+
+    return (
+      <div className="App">
+        <h1 className="top">Active tasks: { activeTasks.length }</h1>
+        {tasks.map(task =>
+          <Task task={task} key={task.id}></Task>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
